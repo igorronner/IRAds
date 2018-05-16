4511615e4b3351289135f945fcbd8108c8c4238c
Igor Ronner <igorronner@gmail.com>
2018-05-16 09:30:23 -0300

    Ajust Interstitial

--- irinterstitial/src/main/java/com/igorronner/irinterstitial/services/IRInterstitialService.kt
+++ irinterstitial/src/main/java/com/igorronner/irinterstitial/services/IRInterstitialService.kt
@@ -1,11 +1,13 @@
 package com.igorronner.irinterstitial.services
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.support.v4.app.ActivityCompat
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
+import com.igorronner.irinterstitial.R
 import com.igorronner.irinterstitial.init.ConfigUtil
 
 open class IRInterstitialService(val activity: Activity) {
@@ -32,6 +34,11 @@ open class IRInterstitialService(val activity: Activity) {
             mInterstitialAd.show()
 
             mInterstitialAd.adListener = object : AdListener() {
+
+                override fun onAdFailedToLoad(p0: Int) {
+                    activity.finish()
+                }
+
                 override fun onAdClosed() {
                     activity.finish()
                 }
@@ -45,6 +52,12 @@ open class IRInterstitialService(val activity: Activity) {
     }
 
     fun showInterstitialBeforeIntent(activity: Activity, intent: Intent, finishAll: Boolean) {
+        val dialog= ProgressDialog(activity)
+        dialog.setMessage(activity.getString(R.string.loading))
+        dialog.setCancelable(false)
+        dialog.isIndeterminate=true
+        dialog.show()
+
         mInterstitialAd?.let {mInterstitialAd ->
 
             mInterstitialAd.show()
@@ -53,8 +66,11 @@ open class IRInterstitialService(val activity: Activity) {
 
 
                 override fun onAdFailedToLoad(p0: Int) {
+                    if (dialog.isShowing)
+                        dialog.hide()
                     if (finishAll)
                         ActivityCompat.finishAffinity(activity)
+
                     activity.startActivity(intent)
                 }
                 override fun onAdClosed() {
@@ -65,6 +81,8 @@ open class IRInterstitialService(val activity: Activity) {
 
                 override fun onAdLoaded() {
                     super.onAdLoaded()
+                    if (dialog.isShowing)
+                        dialog.hide()
                     mInterstitialAd.show()
                 }
             }
