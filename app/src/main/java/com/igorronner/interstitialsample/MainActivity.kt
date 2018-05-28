59e5a9368e980ed2e1b385317fb4e1f27540e511
DESKTOP-6OJ9SU4\IgorR <igorronner@gmail.com>
2018-05-28 11:46:59 -0300

    Revert commit

--- app/src/main/java/com/igorronner/interstitialsample/MainActivity.kt
+++ app/src/main/java/com/igorronner/interstitialsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.igorronner.interstitialsample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.igorronner.irinterstitial.init.IRAds
 import com.igorronner.irinterstitial.views.PurchaseActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//        IRAds.openSplashScreen(this)
 
 
     }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+//        IRAds.showInterstitalOnFinish(this)
+    }
 
 }
--- app/src/main/java/com/igorronner/interstitialsample/MainApp.kt
+++ app/src/main/java/com/igorronner/interstitialsample/MainApp.kt
@@ -7,11 +7,15 @@ class MainApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
 //        IRAds.startInit(this)
-//                .setInterstitialId("PLACE_YOUR_ADD_UNIT_ID")
+//                //Release
+//                //.setInterstitialId("ca-app-pub-3729108735838828/8155983590")
+//                //Test
+//                .setInterstitialId("ca-app-pub-3940256099942544/1033173712")
+//                .setNativeAdId("ca-app-pub-3729108735838828/5447099852")
 //                .setLogo(R.mipmap.ic_launcher)
-//                .setNativeAdId("PLACE_YOUR_NATIVE_AD_ID")
+//                .setAppPrefix("lib_")
+//                //.enablePurchace("premium")
 //                .build()
 
     }
