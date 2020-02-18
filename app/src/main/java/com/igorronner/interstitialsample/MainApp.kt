fb70e7a6c0e13dddf6a2a3ae9f51aabd25fc4b81
Igor Ronner <igorrronner@gmail.com>
2020-02-17 23:17:15 -0300

    Modificações do MainApp do SampleProject

--- app/src/main/java/com/igorronner/interstitialsample/MainApp.kt
+++ app/src/main/java/com/igorronner/interstitialsample/MainApp.kt
@@ -18,8 +18,8 @@ class MainApp : Application() {
                 .setTwoFloorsInterstitial("ca-app-pub-3940256099942544/1033173712",
                         "ca-app-pub-3940256099942544/1033173712")
                 .setNativeAdId("ca-app-pub-3940256099942544/2247696110")
-                .setTwoFloorsNativeAd("ca-app-pub-39402",
-                        "ca-app-pub-3940256099942544/22")
+                .setTwoFloorsNativeAd("ca-app-pub-3940256099942544/2247696110",
+                        "ca-app-pub-3940256099942544/2247696110")
                 .setBannerAdId("ca-app-pub-3940256099942544/6300978111")
                 .setRewardedVideoId("ca-app-pub-3940256099942544/5224354917")
                 .setAppPrefix("lib_")
