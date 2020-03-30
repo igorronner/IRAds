47d58f39538dca1f0bf7c7014d6d73bff2f0254f
Igor Ronner <igorrronner@gmail.com>
2020-03-30 17:11:01 -0300

    Atualização de libs

--- irinterstitial/src/main/java/com/igorronner/irinterstitial/views/SplashActivity.kt
+++ irinterstitial/src/main/java/com/igorronner/irinterstitial/views/SplashActivity.kt
@@ -14,7 +14,6 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var adsInstance: IRAds
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
